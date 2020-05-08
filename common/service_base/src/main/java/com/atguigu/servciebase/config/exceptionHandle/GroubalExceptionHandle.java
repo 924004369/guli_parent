449f8be8bac760dfa2b09f68542f058265e73c80
92400 <924004369@qq.com>
2020-05-08 09:09:19 +0800

    加入logback日志显示

--- common/service_base/src/main/java/com/atguigu/servciebase/config/exceptionHandle/GroubalExceptionHandle.java
+++ common/service_base/src/main/java/com/atguigu/servciebase/config/exceptionHandle/GroubalExceptionHandle.java
@@ -1,6 +1,7 @@
 package com.atguigu.servciebase.config.exceptionHandle;
 
 import com.atguigu.commonutils.ResultMap;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,14 +12,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @date 2020/5/7 16:53
  */
 @ControllerAdvice
+@Slf4j
 public class GroubalExceptionHandle {
-
+    /**
+     * 全局异常
+     * @param e
+     * @return
+     */
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public ResultMap error(Exception e){
         e.printStackTrace();
         return ResultMap.error().message("执行了全局异常处理");
     }
+
+    /**
+     * 特定异常
+     * @param e
+     * @return
+     */
     @ExceptionHandler(ArithmeticException.class)
     @ResponseBody
     public ResultMap error01(Exception e){
@@ -26,9 +38,15 @@ public class GroubalExceptionHandle {
         return ResultMap.error().message("!!!!!!!!!!!!!");
     }
 
+    /**
+     * 自定义异常
+     * @param e
+     * @return
+     */
     @ExceptionHandler(GuliException.class)
     @ResponseBody
     public ResultMap error02(GuliException e){
+        log.error(e.getMessage());
         e.printStackTrace();
         return ResultMap.error().code(e.getCode()).message(e.getMsg());
     }
--- service/service_edu/src/main/java/com/atguigu/serviceedu/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/serviceedu/controller/EduTeacherController.java
@@ -10,7 +10,6 @@ import com.atguigu.serviceedu.service.EduTeacherService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import net.bytebuddy.implementation.bytecode.Throw;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
