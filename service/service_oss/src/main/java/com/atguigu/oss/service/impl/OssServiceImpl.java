cc9d407829fd26ba87eea3674e6adea90f14a622
92400 <924004369@qq.com>
2020-05-09 09:28:07 +0800

    优化oss上传，分类保存图片

--- service/service_oss/src/main/java/com/atguigu/oss/service/impl/OssServiceImpl.java
+++ service/service_oss/src/main/java/com/atguigu/oss/service/impl/OssServiceImpl.java
@@ -7,8 +7,9 @@ import com.atguigu.oss.utils.ConstantPropertiesUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * @author chairuntao
@@ -27,16 +28,29 @@ public class OssServiceImpl implements OssService {
 
 
         try {
+            String fileName=file.getOriginalFilename();
+            final String uuid = UUID.randomUUID().toString().replaceAll("-","");//把uuid中的“-”去除
+            fileName=uuid+fileName;
+
+
+            //在oss上传中创建文件夹，根据文件夹保存
+            final Date date = new Date();
+            final int year = date.getYear()+1900;
+            final int month = date.getMonth()+1;
+            final int day = date.getDate();
+            String datePath=""+year+"/"+month+"/"+day;
+            fileName=datePath+"/"+fileName;
+
             final OSS build = new OSSClientBuilder().build(endPoint, keyId, keySecret);
             final InputStream inputStream = file.getInputStream();
             //第二个参数是  oss文件路径和文件名称
             //第三个参数是文件输入流
-            build.putObject(bucketName, file.getOriginalFilename(), inputStream);
+            build.putObject(bucketName, fileName, inputStream);
 
             build.shutdown();
             String url="";
 //            https://crt-edu.oss-cn-shanghai.aliyuncs.com/20150919092957_J3NQT.jpeg
-            url="https://"+bucketName+"."+endPoint+"/"+file.getOriginalFilename();
+            url="https://"+bucketName+"."+endPoint+"/"+fileName;
             return url;
         } catch (Exception e) {
             e.printStackTrace();
